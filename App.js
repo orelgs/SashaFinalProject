c9e28c4bfec9b5497296ec41b3f24713c0be0e55
sashakolovarsky <118433604+sashakolovarsky@users.noreply.github.com>
2024-06-06 00:31:15 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PickerScreen from './PickerScreen';
+import SliderScreen from './SliderScreen';
+import HomeScreen from './HomeScreen';
+import ThankYouScreen from './ThankYouScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Question1" component={PickerScreen} 
+            initialParams={{ question: 'When did you go to sleep?',
+            nextScreen: 'Question2',
+            defaultHour: "22",
+            defaultMinute: "00" }} />
+        <Stack.Screen name="Question2" component={PickerScreen}
+            initialParams={{ question: 'When did you wake up?',
+            nextScreen: 'Question3',
+            defaultHour: "08",
+            defaultMinute: "00" }} />
+        <Stack.Screen name="Question3" component={SliderScreen}
+            initialParams={{ question: 'How good was your sleep?',
+            nextScreen: 'Question4' }} />
+        <Stack.Screen name="Question4" component={SliderScreen}
+            initialParams={{ question: 'How do you feel now? Rate your mood.',
+            nextScreen: 'Question5' }} />
+        <Stack.Screen name="Question5" component={PickerScreen}
+            initialParams={{ question: 'When was your first meal yesterday?',
+            nextScreen: 'Question6',
+            defaultHour: "09",
+            defaultMinute: "00" }} />
+        <Stack.Screen name="Question6" component={PickerScreen}
+            initialParams={{ question: 'When was your last meal yesterday?',
+            nextScreen: 'ThankYou',
+            defaultHour: "21",
+            defaultMinute: "00" }} />
+        <Stack.Screen name="ThankYou" component={ThankYouScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ HomeScreen.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+const logo = require('./assets/DataRhythmLogo.jpg');
+
+export default function HomeScreen({ navigation }) {
+
+  const username = "User"; // Replace with actual username
+
+  const handleStartAnswering = () => {
+    navigation.navigate("Question1");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} resizeMode='contain' />
+      <Text style={styles.welcomeText}>Hello {username}!</Text>
+      <TouchableOpacity onPress={handleStartAnswering} style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Start Answering</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start', 
+    alignItems: 'center',
+    backgroundColor: 'black',
+    paddingTop: 20, 
+  },
+  welcomeText: {
+    fontSize: 40,
+    color: "white",
+    marginBottom: 20,
+  },
+  logo: {
+    width: 350,
+    marginBottom: 10, 
+  },
+  buttonContainer: {
+    backgroundColor: '#00B5B9',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#00B5B9',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 10,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+});
